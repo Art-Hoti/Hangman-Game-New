9b64612d1502f2e117988cf3551249ae0e96e62f
Art Hoti <mrhoti@Arts-MacBook-Pro.local>
2024-07-15 17:19:49 +0100

    Update latest code changes

---
+++ milestone_2.py
@@ -0,0 +1,17 @@
+import random
+
+favourite_countries = ["United Kingdom", "Canada", "Australia", "New Zealand", "India"]
+word_list = favourite_countries
+
+print(word_list)
+
+word = random.choice(word_list)
+print(word)
+
+guess = input("Please enter a single letter: ")
+print("You entered:", guess)
+
+if len(guess) == 1 and guess.isalpha():
+    print("Good guess!")
+else:
+    print("Oops! That is not a valid input.")
---
+++ module_1.py
@@ -0,0 +1,4 @@
+def function_package():
+    print("I am in function_package() in package_1/module_1.py")
+    from module_1 import function_1
+
